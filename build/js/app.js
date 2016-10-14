f3a422562603cb52e4147f43595ee3ac53b688ad
Caleb Paul <calebpaul21@gmail.com>
2016-10-14 11:59:31 -0700

    add prototype for getting user repos

--- build/js/app.js
+++ build/js/app.js
@@ -9,17 +9,24 @@ function Lookup() {
 }
 // https://api.github.com/users/daneden?access_token=750272a02e5a4bbc131e56a790127a67311a1605
 
+Lookup.prototype.getRepos = function(repoUrl){
+  $.get(repoUrl).then(function(response){
+    console.log(response);
+
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
+
 Lookup.prototype.getUser = function(user){
+  var that = this;
   $.get('https://api.github.com/users/'+user+'?access_token=' + apiKey).then(function(response){
     console.log(response);
-    //access object properties
     var name = response.name;
     var location = response.location;
     var repos = response.public_repos;
     var githubUrl = response.url;
     var repoUrl = response.repos_url;
-    //append repos after url value? could use for of loop?
-    //catch null
     if (name === null) {
       name = "user did not give name"
     }
@@ -31,11 +38,12 @@ Lookup.prototype.getUser = function(user){
     console.log(repos);
     console.log(githubUrl);
     console.log(repoUrl);
-    $('.info').append('<div class="col-sm-5 text-center">'
+    that.getRepos(repoUrl);
+    $('.info').append('<div class="col-sm-4 text-center">'
     + '<label class = "page-header">User Info</label>'
     + '<h6>User Name: '+name+'</h6>'
     +'<h6>Location: '+location+'</h6>'
-    +'<h6># of projects: '+repos+'</h6>'
+    +'<h6># of repositories: '+repos+'</h6>'
     +'</div>');
 
   }).fail(function(error){
